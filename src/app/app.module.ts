990dbb281f3a507dde17cef703875460073b2cc8
yAshitaba <tianlin0515@gmail.com>
2018-09-02 08:35:54 +0900

    implements bookmark function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomePage } from '../pages/home/home';
 import { EventProvider } from '../providers/event/event';
 import { HttpClientModule } from '@angular/common/http';
 import { BookmarkProvider } from '../providers/bookmark/bookmark';
+import { IonicStorageModule } from '@ionic/storage';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { BookmarkProvider } from '../providers/bookmark/bookmark';
   imports: [
     BrowserModule,
     HttpClientModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { EventProvider } from '../../providers/event/event';
+import { BookmarkProvider } from '../../providers/bookmark/bookmark';
 
 /**
  * Generated class for the EventDetailPage page.
@@ -19,7 +20,13 @@ import { EventProvider } from '../../providers/event/event';
 export class EventDetailPage {
 
   event: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public eventProvider: EventProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public eventProvider: EventProvider,
+    public toastCtrl: ToastController,
+    public bookmarkProvider: BookmarkProvider
+  ) {
   }
 
   ionViewDidLoad() {
@@ -32,4 +39,13 @@ export class EventDetailPage {
     }
   }
 
+  doBookmark() {
+    this.bookmarkProvider.put(this.event).then(() => {
+      const toast = this.toastCtrl.create({
+        message: 'イベントをブックマークしました。',
+        duration: 1500
+      });
+      toast.present();
+    })
+  }
 }
